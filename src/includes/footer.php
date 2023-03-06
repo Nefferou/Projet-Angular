45c2fe08f70c254f41ccc363f4bc44d18ff0a986
Nefferou <fertijulien@gmail.com>
2023-03-06 16:16:30 +0100

    Construction des pages

---
+++ src/includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="footer">
+        <p>Copyright © 2023 - <a href="#">Mon entreprise</a></p>
+    </div>
+</footer>
---
+++ src/includes/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Groupe B">
+    <title>Projet NoSQL</title>
+    <link rel="stylesheet" href="style.css">
+</head>
