217458c67870b48ab77becf08601071df7d86bc1
Hugogoncalves06 <hugo.goncalves06@ynov.com>
2023-03-20 12:26:10 +0100

    mongodb connection problem

--- src/back/connexionbdd.php
+++ src/back/connexionbdd.php
@@ -1,5 +1,5 @@
 <?php
-$collection = (new MongoDB\Client)->admin->system->users;
-echo $collection->find();
+$collection = new \MongoDB\Client("mongodb://vps422793.ovh.net:27017");
+echo $collection;
 
 ?>
