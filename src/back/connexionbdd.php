db688b4be62c2997baf7d7d329d195278ffdc171
Hugogoncalves06 <hugo.goncalves06@ynov.com>
2023-03-24 21:32:23 +0100

    connexion Antho

--- src/back/connexionbdd.php
+++ src/back/connexionbdd.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../../vendor/autoload.php';
+require '/var/www/html/vendor/autoload.php';
 $client=new MongoDB\Client("mongodb://root:GroupeB@mongodb:27017");
 $db=$client->GroupeB;
 $collections = $db->listColle->listCollections();
