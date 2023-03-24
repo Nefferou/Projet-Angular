969b42925d304bd8b74f57a3dddf05285e0eabae
Lilian Chardon <55657944+Tequhella@users.noreply.github.com>
2023-03-24 21:04:00 +0100

    ajout tri restau

--- src/back/connexionbdd.php
+++ src/back/connexionbdd.php
@@ -1,5 +1,8 @@
 <?php
-$collection = new MongoClient();
-echo $collection;
+
+require_once '../../vendor/autoload.php';
+
+$client = new MongoDB\Client(
+      'mongodb://Tequhella:Communism06!@127.0.0.1:27017');
 
 ?>
