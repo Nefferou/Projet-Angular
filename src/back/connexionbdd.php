0ecd4bc2afb2bd60d4af1559035b29eeaeb00bb3
Hugogoncalves06 <hugo.goncalves06@ynov.com>
2023-03-24 21:39:12 +0100

    show user collection

--- src/back/connexionbdd.php
+++ src/back/connexionbdd.php
@@ -2,7 +2,6 @@
 
 require '/var/www/html/vendor/autoload.php';
 $client=new MongoDB\Client("mongodb://root:GroupeB@mongodb:27017");
-$db=$client->GroupeB;
-$collections = $db->listColle->listCollections();
-echo $collections;
+$collection=$client->selectCollection('GroupeB','users');
+echo $collection;
 ?>
