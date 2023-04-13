20c05038cf569407480672074b93a1ea80eb2aa7
Hugogoncalves06 <hugo.goncalves06@ynov.com>
2023-04-14 00:23:18 +0200

    V.1.0 - call api

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -8,10 +8,32 @@ import { PokemonService } from '../Service/pokemon.service';
   styleUrls: ['./pokemon-card.component.scss']
 })
 export class PokemonCardComponent {
+
   @Input() pokemon!:Pokemon;
 
   selectedPrice: number = 0;
 
   constructor(private pokemonService: PokemonService) { }
 
+  addPokemon(pokemon: Pokemon) {
+    console.log(pokemon.name+" ADDED")
+  }
+
+  removePokemon(pokemon: Pokemon) {
+    console.log(pokemon.name+" REMOVED")
+
+  }
+
+  Favorite(pokemon: Pokemon) {
+    (pokemon.isLikes)? pokemon.isLikes = false : pokemon.isLikes = true;
+    }
+
 }
+/**
+ *       <button class="add" (click)="addPokemon(pokemon)">+</button>
+      <button class="remove" (click)="removePokemon(pokemon)">-</button>
+    </div>
+    <div class="favorites">
+      <button class="favorite" (click)="addFavorite(pokemon)">♥</button>
+      <button class="unfavorite" (click)="removeFavorite(pokemon)">♡</button>
+ */
